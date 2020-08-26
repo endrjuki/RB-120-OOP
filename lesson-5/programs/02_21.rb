1f624196d66d68cb140df84a86881e7718fd5314
endrjuki <endrju.vilcmeiers@gmail.com>
2020-08-26 12:09:57 +0300

    Now deals initial cards, displays the status of table accurately(displays only one of the dealers cards and keeps score secret))

--- lesson-5/programs/02_21.rb
+++ lesson-5/programs/02_21.rb
@@ -83,11 +83,18 @@ class Card
 end
 
 class Game
-  attr_accessor :human, :dealer, :deck
+  attr_accessor :player, :dealer, :deck
 
   MAX_SCORE = 21
 
+  def initialize
+    @deck = Deck.new
+    @player = Player.new("Bobby")
+    @dealer = Dealer.new("Holmes")
+  end
+
   def display_welcome_message
+    system "clear"
     puts "Welcome to 21"
     puts "PLACEHOLDER"
     sleep(1)
@@ -97,26 +104,34 @@ class Game
     puts "Thanks for playing 21. Goodbye!"
   end
 
-  def display_table
-    puts "----------"
-    puts "Player's hand:"
-    puts "#{display_cards(@human)}"
-    puts "#{@human.calculate_score}"
-    puts "----------"
-    puts "Computer's hand:"
-    puts "#{display_cards(@dealer)}"
-    puts "<COMPUTER SCORE HERE: ?>"
+  def dealer_hand_helper_method(dealer_hand)
+    case dealer_hand
+    when :visible
+      dealer_score = dealer.calculate_score
+      dealer_cards = display_cards(dealer, :display_all)
+    when :invisible
+      dealer_score = '???'
+      dealer_cards = display_cards(dealer, :display_only_first)
+    end
+    [dealer_score, dealer_cards]
   end
 
-  def initialize
-    @deck = Deck.new
-    @human = Player.new("Bobby")
-    @dealer = Dealer.new("Holmes")
+  def display_table(dealer_hand=:visible)
+    dealer_score, dealer_cards = dealer_hand_helper_method(dealer_hand)
+
+    puts "----------"
+    puts "Player's hand:"
+    puts display_cards(player)
+    puts "Score: #{player.calculate_score}"
+    puts "----------"
+    puts "Dealer's hand:"
+    puts dealer_cards
+    puts "Score: #{dealer_score}"
   end
 
   # ------- PLAYER TURN
   def deal_card(player)
-    player.hand << @deck.deal
+    player.hand << deck.deal
   end
 
   def hit_or_stay
@@ -130,7 +145,7 @@ class Game
       puts "Invalid choice!"
     end
 
-    choice
+    choice #returns string 'h' or 's'
   end
 
   def player_busted?(player)
@@ -138,62 +153,39 @@ class Game
   end
 
   def player_turn
-    loop do
-      deal_card(@human)
-      display_table
-      break if player_busted?(@human)
-      case hit_or_stay
-      when 'h'
-        system "clear"
-        puts "Player choses to hit!"
-        next
-      when 's'
-        system "clear"
-        puts "Player choses to stay!"
-        sleep(1)
-        break
-      end
-    end
-
-    if player_busted?(@human)
-      puts "You busted! Computer wins!"
-      sleep(1)
-    end
 
   end
 
   def computer_turn
-    loop do
-      system "clear"
-      display_table
-      sleep 1
-      deal_card(@dealer)
-      break if @dealer.hand.length > 4
-    end
+
   end
 
-  def display_cards(player)
+  def display_cards(player, amount=:display_all)
     result = []
 
     player.hand.each do |card|
       result.push("#{card.suit} #{card.rank}")
     end
 
-    result.join(', ')
+    amount == :display_all ? cards = result.size : cards = 1
+    result[0, cards].join(', ')
+  end
+
+  def deal_initial_cards
+    2.times do
+      deal_card(player)
+      deal_card(dealer)
+    end
+  end
+
+  def show_initial_cards
+    display_table(:invisible)
   end
 
   def start
     display_welcome_message
     deal_initial_cards
     show_initial_cards
-
-    player_turn
-
-    computer_turn
-
-    #setup_game
-
-    #show_initial_cards
     #player_turn
     #dealer_turn
     #show_result
